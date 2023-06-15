c363ad43e596df6acc909eb8c969bcf68a224b36
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-15 16:43:55 +0200

    Layout generico header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,10 +6,62 @@ export default{
 </script>
 
 <template>
+    <header>
+        <div class="container">
+            <div class="box-img">
+                <img src="../assets/img/dc-logo.png" alt="DC logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
     
 
 </template>
 
-<style>
+<style lang="scss">
+header{
+    background-color: red;
+}
+
+div.container{
+    display: flex;
+    justify-content: space-between;
+
+        & ul{
+            display: flex;
+        };
+
+        & li{
+            border-bottom: 3px solid black;
+            padding:0 0.5rem 2rem;
+            margin: 0 0.5rem;
+        }
+}
+
+div.box-img{
+    padding: 0.5rem 0;
+
+    img{
+        height: 80px;
+    }
+}
+
+nav{
+    align-self: flex-end;
+}
 
 </style>
